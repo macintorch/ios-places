34e69a863a8c4edf6404d0cd40bb8412a90a452c
Ainor Syahrizal <macintorch@gmail.com>
2017-09-06 09:43:25 +0800

    get place details to display on map

--- Places/ViewController.swift
+++ Places/ViewController.swift
@@ -18,6 +18,41 @@ class ViewController: UIViewController, MKMapViewDelegate {
         // Do any additional setup after loading the view, typically from a nib.
         
         print(activePlace)
+        
+        if activePlace != -1 {
+            // get place details to display on map
+            
+            if places.count > activePlace {
+                
+                if let name = places[activePlace]["name"] {
+                    if let lat = places[activePlace]["lat"] {
+                        if let lon = places[activePlace]["lon"] {
+                            if let latitude = Double(lat) {
+                                
+                                if let longitude = Double(lon) {
+                                    
+                                     let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+                                    
+                                    let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+                                    
+                                    let region = MKCoordinateRegion(center: coordinate, span: span)
+                                    
+                                    self.map.setRegion(region, animated: true)
+                                    
+                                    let annotation = MKPointAnnotation()
+                                    
+                                    annotation.coordinate = coordinate
+                                    
+                                    annotation.title = name
+                                    
+                                    self.map.addAnnotation(annotation)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
